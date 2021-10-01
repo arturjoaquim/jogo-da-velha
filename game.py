563e074897222d51d52c456c74ae9a735f6c59f5
Artur Joaquim <artur.joaquimbr@gmail.com>
2021-10-01 18:05:56 -0300

    Edit class Player and create class GameValidations

--- game.py
+++ game.py
@@ -14,41 +14,28 @@ class Game:
         squares_points     (iterable): Número da conta do cliente.
     '''
 
-    field_points = [2, 7, 6, 9, 5, 1, 4, 3, 8]
-
-
-    @staticmethod
-    def powerset(iterable): # Retorna um objeto com todas as combinações dos valores de um iterable.
-        "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-        s = list(iterable)
-        return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
-
+    points = [2, 7, 6, 9, 5, 1, 4, 3, 8]
 
     def __init__(self):
-        self.field_values = [None, None, None, None, None, None, None, None, None]
-        self.game_round = 0
+        self.fields = [None, None, None, None, None, None, None, None, None]
         self.players = []
-        self.winner = None
-        self.circles_points = []
-        self.squares_points = []
+        self.game_round = 0
+        # self.circles_points = []
+        # self.squares_points = []
 
+    # def add_player(self, player):
+    #     assert len(self.players) < 2
+    #     self.players.append(player)
+    #     print(f"Player {player.get_player_name()} foi adicionado ao jogo!")
 
-    def add_player(self, player):
-        assert len(self.players) < 2
-        self.players.append(player)
-        print(f"Player {player.get_player_name()} foi adicionado ao jogo!")
-
-
-    def _set_players_team(self):
+    def _set_players_team(self): # Mudar funcionalidade
         self.players[0].team = "Circles"
         self.players[1].team = "Squares"
 
-
-    def get_player_by_id(self, id_player):
-        for player in self.players:
-            if player.id == id_player:
-                return player
-
+    # def get_player_by_id(self, id_player):
+    #     for player in self.players:
+    #         if player.id == id_player:
+    #             return player
 
     def show_rating(self):
         for player in self.players:
@@ -58,25 +45,20 @@ class Game:
             print("{}".format(f"Derrotas: {player.defeats}"))
             print("{:-^40s}".format("-"))
 
+    # def start_game(self):
+    #     self._set_players_team()
+    #     print("O jogo começou!")
 
-    def start_game(self):
-        self._set_players_team()
-        print("O jogo começou!")
+    def _add_point(self, player, field_index):
+        point = self.points[field_index]
+        player.points.append(point)
+        self.fields[field_index] = player.team
 
-
-    def _add_point(self, team, field):
-        if player.team == "Circles":
-            self.circles_points.append(self.field_points[field])
-        elif player.team == "Squares":
-            self.squares_points.append(self.field_points[field])
-
-
-    def player_move(self, player, field):
-        field = int(field)
-        self.field_values[field] = player.team
-        self._add_point(player, field)
-        print(f"O campo {field} foi marcado para o time {player.team} com sucesso.")
-
+    # def player_move(self, player, field):
+    #     field = int(field)
+    #     self.field_values[field] = player.team
+    #     self._add_point(player, field)
+    #     print(f"O campo {field} foi marcado para o time {player.team} com sucesso.")
 
     def _is_winner(self, points_list):
         if self.game_round >= 3:
@@ -86,22 +68,19 @@ class Game:
                     if sum(combination) == 15:
                         return True 
 
-
-    def check_winner(self):
-        players_scores = [self.circles_points, self.squares_points]
-        for index, player_score in enumerate(players_scores):
-            print(index, player_score)
-            if self._is_winner(player_score):
-                self.winner = self.get_player_by_id(index + 1)
-                return True
-
+    # def check_winner(self):
+    #     players_scores = [self.circles_points, self.squares_points]
+    #     for index, player_score in enumerate(players_scores):
+    #         print(index, player_score)
+    #         if self._is_winner(player_score):
+    #             self.winner = self.get_player_by_id(index + 1)
+    #             return True
 
     def start_round(self):
         self.game_round += 1
         print("{:-^60s}".format("-"))
         print("{:*^60s}".format(f"Round {self.game_round}"))
         print("{:-^60s}".format("-"))
-
 
     def end_round(self):
         if self.winner:
@@ -119,19 +98,70 @@ class Game:
             return True
 
 
-class Player:
+class GameValidations():
 
-    id = 1
+    @staticmethod
+    def powerset(iterable): # Retorna um objeto com todas as combinações dos valores de um iterable.
+        "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+        s = list(iterable)
+        return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
 
-    def __init__(self, name, game):
-        self.id = Player.id
-        self.name = name
-        self.team = None
+    def __init__(self, game) -> None:
+        self.game = game
+
+    def validate_field_index(self, field_index):
+        try:
+            field_index = int(field_index)
+        except (ValueError):
+            print("Digite valores inteiros para preencher um campo.")
+            return False
+
+        if field_index >= 0 and field_index <= 8:
+            if not self.game.fields[field_index]:
+                return True
+            else:
+                print("Este campo já foi preenchido.")
+                return False
+        else:
+            print("O valor do campo precisa estar entre 0 e 8.")
+            return False
+
+    def is_winner(self, points_list):
+        # if self.game_round >= 3:
+        combinations_list = self.powerset(points_list)
+        for combination in combinations_list:
+            if len(combination) == 3:
+                if sum(combination) == 15:
+                    return True 
+
+
+    # def validate_player_name(player_name):
+    #     if len(player_name) >= 3 and len(player_name) <= 20:
+    #         if player_name.isalnum():
+    #             return True
+    #         else:
+    #             print("Todos os caracteres precisam ser alfanuméricos.")
+    #     else:
+    #         print("Seu nome precisa ter de 3 à 20 caracteres.")
+    #         return False
+
+
+class Player():
+
+    def __init__(self, game):
+        self.game = game
+        self.points = []
+        self.team = "None"
         self.wins = None
         self.defeats = None
-        Player.id += 1
-        game.add_player(self)
+        game.players.append(self)
 
-    def get_player_name(self):
-        name = self.name
-        return str(name)
+    def move(self, field_index):
+        # if game_validations.validate_field_index(field_index):
+        field_index = int(field_index)
+        self.game.fields[field_index] = self.team
+        self.game._add_point(self, field_index)
+        print(
+            (f"O campo {field_index} foi marcado para o" +
+                f"time {self.team} com sucesso.")
+        )
--- main.py
+++ main.py
@@ -1,41 +1,44 @@
-from game import Game, Player
-from validations import validate_field, validate_player_name
-
-game = Game()
-
-for i in range(2):
-	while True:
-		name_player = input(f"Digite o nome do player {i + 1}: ")
-		if validate_player_name(name_player):
-			player = Player(name_player, game)
-			break
+from game import Game, GameValidations, Player
 
 
 
-game.start_game()
+def main():
+    game = Game()
+    game_validations = GameValidations(game)
+    player = Player(game)
+
+    # for i in range(2):
+    #     while True:
+    #         name_player = input(f"Digite o nome do player {i + 1}: ")
+    #         if validate_player_name(name_player):
+    #             player = Player(name_player, game)
+    #             break
+
+    # game.start_game()
+
+    loop = True
+
+    while loop:
+        i = 0
+        game.show_rating()
+        while i < 9:
+            game.start_round()
+            for player in game.players:
+                while True:
+                    print("{:-^40s}".format("-"))
+                    print("{:*^40s}".format(f"Jogador {player.id}"))
+                    print("{:-^40s}".format("-"))
+                    field_index = input("Digite um campo de 0 à 8 para marcar.")
+
+                    if game_validations.validate_field_index(field_index):
+                        game.player_move(player, field_index)
+                        i += 1
+                        break
+                    else:
+                        pass
+                if game.check_winner(): break
+            if game.end_round(): break
+        loop = game.end_game()
 
 
-
-loop = True
-
-while loop:
-	i = 0
-	game.show_rating()
-	while i < 9:
-		game.start_round()
-		for player in game.players:
-			while True:
-				print("{:-^40s}".format("-"))
-				print("{:*^40s}".format(f"Jogador {player.id}"))
-				print("{:-^40s}".format("-"))
-				field = input("Digite um campo de 0 à 8 para marcar.")
-
-				if validate_field(game.field_values, field):
-					game.player_move(player, field)
-					i += 1
-					break
-				else:
-					pass
-			if game.check_winner(): break
-		if game.end_round(): break
-	loop = game.end_game()
+main()
